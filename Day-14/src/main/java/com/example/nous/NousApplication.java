fdff8605bfb13ebd4dba418fd6227928a45a1732
Sharan <110533772+sharanshanmugam7@users.noreply.github.com>
2023-10-05 16:00:40 +0530

    Add files via upload

---
+++ Day-14/src/main/java/com/example/nous/NousApplication.java
@@ -0,0 +1,13 @@
+package com.example.nous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NousApplication.class, args);
+	}
+
+}
